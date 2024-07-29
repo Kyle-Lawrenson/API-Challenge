01ad771a70ec57725f4cab1ebc2b39fa2d3db886
Kyle Lawrenson <kylelawrenson99@gmail.com>
2024-07-29 16:36:17 +0100

    functional plus some css

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,53 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import './App.css'
 
 const App = () => {
   const [allFilms, setAllFilms] = useState([]);
-  const [errorMsg, setErrorMsg] = ("");
+  const [errorMsg, setErrorMsg] = useState("");
+  const [open, setOpen] = useState(false);
+  const [selected, setSelected] = useState({})
 
   const fetchData = async () => {
-    try{
+    try {
       const response = await fetch("https://ghibliapi.vercel.app/films")
 
       if (!response.ok) {
         throw new Error("An error has occured")
       }
 
-    const filmsData = await response.json()
-    setAllFilms(filmsData)
-    setErrorMsg("")
+      const filmsData = await response.json()
+      setAllFilms(filmsData)
+      setErrorMsg("")
     } catch (error) {
       console.log(error.message)
-      setErrorMsg(error.message)  
+      setErrorMsg(error.message)
     }
   }
 
-useEffect(() => {
-  fetchData()
-}, [])
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  const Modal = () => {
+    return (
+      <div>
+        <p>{film.description}</p>
+      </div>
+    )
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleClick = (film) => {
+    setOpen(true);
+      // console.log(film.description)
+      setSelected(film)
+
+      // useRef(film.description)
+      // {film.description}
+  };
 
   return (
     <>
@@ -33,15 +56,22 @@ useEffect(() => {
       {errorMsg !== "" && (
         <p>{errorMsg}</p>
       )}
-
+  <div id='filmContainer'>
       {allFilms.map((film, index) => {
         return (
-          <div className='films' key = {index}>
-          <h3> {film.title}</h3>
-          <img src={film.image}></img>
+          <div className='films' key={index}>
+            {/* <h3 id='title'> {film.title}</h3> */}
+            <img className='img' onClick={() => handleClick(film)} src={film.image}></img>
           </div>
         )
       })}
+      </div>
+      {open && (
+        <div className='modal'>{/* add ref */}
+          <p>{selected.description}</p>
+          <button id='close' onClick={handleClose}>Close</button>
+        </div>
+      )}
     </>
   )
 }
